630ceaff71359bacb1f6a636e65fce8a4048ae0f
ttcdm <titus.gamerfpv@gmail.com>
2025-01-15 03:52:21 -0500

    echo.asm and helloworld.asm

--- examples/helloworld.asm
+++ examples/helloworld.asm
@@ -1,47 +1,24 @@
 [org 0x7C00]
-;must use bx for general data access?
-mov bx, var
+
+;teletype mode
+
+mov ah, 0x0e
+
+mov bx, x
+
+
 other:
-	mov ah, 0x00;key press mode or something
-	int 0x16
-	cmp ah, 0x1c
-	je mvbx
-	call buffer
-	
-	mov ah, 0x0e
-	int 0x10
-	jmp other
-
-buffer:;apparently i had to put this under where the call was but idk?
-	
-	mov [bx], al
-	inc bx
-	ret
-
-
-mvbx:
-
-	mov bx, var
-	mov ah, 0x0e
-	mov al, 0x0a
-	int 0x10
-	mov al, 0x0d
-	int 0x10
-	jmp print
-print:
 	mov al, [bx];this is because we need an external variable to increment it. it's like when you write a while loop and you need to initialize a variable outside of the loop in order to stop it because if you intialize it inside the loop it just starts over and over again
-	cmp al, 0x00
+	cmp al, 0x0
 	je exit
 	int 0x10
 	inc bx
-	jmp print
+	jmp other
 	
-
-
 exit:
 	jmp $
-var:
-	times 10 db 0
+x:
+	db "hellworld", 0
 
 jmp $; $ is current line and $$ is current sector or something
 
