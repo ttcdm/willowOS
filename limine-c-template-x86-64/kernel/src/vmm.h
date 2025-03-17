ffaf6c7d47e0023ef4004c0c2d4af7a04d62bad1
ttcdm <titus.gamerfpv@gmail.com>
2025-03-16 20:47:26 -0400

    changes from mac

--- limine-c-template-x86-64/kernel/src/vmm.h
+++ limine-c-template-x86-64/kernel/src/vmm.h
@@ -1,6 +1,14 @@
+// #pragma once
+
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
 
 #include <kutils.h>
+#include <paging.c>
+
+#define HEAP_START_VIRT_DEFINED 0xfffc000000000000
+
+
+uint64_t init_heap();
 
