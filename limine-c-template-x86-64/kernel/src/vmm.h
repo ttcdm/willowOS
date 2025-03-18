b332574a978a797d3d19139d6c7f424b61572379
ttcdm <titus.gamerfpv@gmail.com>
2025-03-18 02:46:51 -0400

    I THINK KMALLOC WORKSSS. added a linked list to keep track of the free 64 byte chunks

--- limine-c-template-x86-64/kernel/src/vmm.h
+++ limine-c-template-x86-64/kernel/src/vmm.h
@@ -8,6 +8,18 @@
 #include <paging.h>
 
 #define HEAP_START_VIRT_DEFINED 0xffffc00000000000
-
+#define HEAP_SIZE_DEFINED 0x400000//16mb of memory for heap
+#define PAGE_SIZE_DEFINED 0x1000
+#define HEAP_CHUNK_SIZE_DEFINED 64
 
 uint64_t init_heap();
+
+uint64_t kmalloc();
+
+typedef struct heap_page_virt {
+    uint8_t status;//0 for free and 1 for used
+    uint64_t size;
+    struct heap_page_virt* next;
+} heap_page;
+
+extern heap_page* heap_page_head;
