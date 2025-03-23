9bd4867817ae154c07f5d272bdeb3d3aff5ec9d2
ttcdm <titus.gamerfpv@gmail.com>
2025-03-23 02:24:40 -0400

    more rsdp stuff

--- limine-c-template-x86-64/kernel/src/apic.c
+++ limine-c-template-x86-64/kernel/src/apic.c
@@ -32,3 +32,79 @@ void enable_lapic(void) {//this can be rewritten to be a lot cleaner but it's ex
 
     __asm__ volatile("wrmsr" : : "a"(msr_low), "d"(msr_high) , "c"(0x1b));
 }
+
+int strncmp(const char* s1, const char* s2, size_t n) {
+    while (n-- > 0) {
+        if (*s1 != *s2) {
+            return (unsigned char)*s1 - (unsigned char)*s2;
+        }
+        if (*s1 == '\0') {
+            return 0;
+        }
+        s1++;
+        s2++;
+    }
+    return 0;
+}
+
+const struct MADT *ACPI_MADT;
+
+static void validate_sdt(const struct SDTHeader *pSDT) {
+    uint8_t checksum = 0;
+    for (size_t i = 0; i < pSDT->length; ++i) {
+        checksum += ((const uint8_t *)pSDT)[i];
+    }
+    //if (checksum) panic("Invalid SDT. Expected 0, got %u\n", checksum);
+    //if (checksum) { kprint("Invalid SDT. Expected 0, got "); kprintln_uint64(checksum); }
+}
+
+static void validate_rsdp(const struct RSDP *pRSDP) {
+    uint8_t checksum = 0;
+    for (size_t i = 0; i < sizeof(struct RSDP); ++i) {
+        checksum += ((const uint8_t *)pRSDP)[i];
+    }
+    //if (checksum) panic("Invalid RSDP. Expected 0, got %u\n", checksum);
+    //if (checksum) { kprint("Invalid RSDP. Expected 0, got "); kprintln_uint64(checksum); }
+}
+
+static void validate_xsdp(const struct XSDP *pXSDP) {
+    uint8_t checksum = 0;
+    for (size_t i = 0; i < sizeof(struct XSDP); ++i) {
+        checksum += ((const uint8_t *)pXSDP)[i];
+    }
+    //if (checksum) { kprint("Invalid XSDP. Expected 0, got "); kprintln_uint64(checksum); }
+    //if (checksum) panic("Invalid XSDP. Expected 0, got %u\n", checksum);
+}
+
+static void parse_sdt(const struct SDTHeader *pSDT) {
+    validate_sdt(pSDT);
+    if (!strncmp(pSDT->signature, "APIC", 4)) {
+        ACPI_MADT = (const void *)pSDT;
+    }
+}
+
+void acpi_parse_rsdp(const void *pRSDP) {
+    const struct XSDP *pXSDP = pRSDP;
+    validate_rsdp(&pXSDP->rsdp);
+
+    bool has_xsdp = pXSDP->rsdp.revision > 0;
+    if (has_xsdp) {
+        validate_xsdp(pXSDP);
+
+        const struct XSDT *pXSDT = pXSDP->xsdt_address+hhdm_offset;
+        validate_sdt(&pXSDT->h);
+
+        const size_t num_sdts = (pXSDT->h.length - offsetof(struct XSDT, sdt64)) / sizeof(uint64_t);
+        for (size_t i = 0; i < num_sdts; ++i) {
+            parse_sdt(pXSDT->sdt64[i]+hhdm_offset);
+        }
+    } else {
+        const struct RSDT *pRSDT = pXSDP->rsdp.rsdt_address+hhdm_offset;
+        validate_sdt(&pRSDT->h);
+
+        const size_t num_sdts = (pRSDT->h.length - offsetof(struct RSDT, sdt32)) / sizeof(uint32_t);
+        for (size_t i = 0; i < num_sdts; ++i) {
+            parse_sdt(pRSDT->sdt32[i]+hhdm_offset);
+        }
+    }
+}
--- limine-c-template-x86-64/kernel/src/apic.h
+++ limine-c-template-x86-64/kernel/src/apic.h
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 
 #include <kutils.h>
+#include <paging.h>
+
 
 
 #define PIC1		0x20		/* IO base address for master PIC */
@@ -22,8 +24,10 @@ static inline uint8_t inb(uint16_t port);
 void pic_disable(void);
 void enable_lapic(void);
 
-typedef struct {//chatgpt generated
-    uint32_t signature;       // "APIC"
+void acpi_parse_rsdp(const void *pRSDP);
+
+struct SDTHeader {
+    char signature[4];
     uint32_t length;
     uint8_t revision;
     uint8_t checksum;
@@ -32,7 +36,73 @@ typedef struct {//chatgpt generated
     uint32_t oem_revision;
     uint32_t creator_id;
     uint32_t creator_revision;
+} __attribute__ ((packed));
 
-    uint32_t lapic_address;    // << The LAPIC base address (usually 0xFEE00000)
+struct RSDP {//thanks to hildorthedorf for the help
+    char signature[8];
+    uint8_t checksum;
+    char OEMID[6];
+    uint8_t revision;
+    uint32_t rsdt_address;
+} __attribute__ ((packed));
+
+struct XSDP {
+    struct RSDP rsdp;
+
+    uint32_t length;
+    uint64_t xsdt_address;
+    uint8_t extended_checksum;
+    uint8_t reserved[3];
+} __attribute__ ((packed));
+
+struct RSDT {
+    struct SDTHeader h;
+    uint32_t sdt32[];
+} __attribute__ ((packed));
+
+struct XSDT {
+    struct SDTHeader h;
+    uint64_t sdt64[];
+} __attribute__ ((packed));
+
+struct MADT {
+    struct SDTHeader h;
+    uint32_t lapic_addr;
     uint32_t flags;
-} MADT;
+    char records[];
+} __attribute__ ((packed));
+
+struct MADTEntryHeader {
+    uint8_t type;
+    uint8_t length;
+} __attribute__ ((packed));
+
+struct MADTProcessorLocalAPIC {
+    struct MADTEntryHeader h;
+    uint8_t processor_id;
+    uint8_t apic_id;
+    uint32_t flags;
+} __attribute__ ((packed));
+
+struct MADTIOAPIC {
+    struct MADTEntryHeader h;
+    uint8_t apic_id;
+    uint8_t reserved;
+    uint32_t address;
+    uint32_t global_system_interrupt_base;
+} __attribute__ ((packed));
+
+struct MADTInterruptSourceOverride {
+    struct MADTEntryHeader h;
+    uint8_t bus_source;
+    uint8_t irq_source;
+    uint32_t global_system_interrupt;
+    uint16_t flags;
+} __attribute__ ((packed));
+
+struct MADTLocalAPICNMI {
+    struct MADTEntryHeader h;
+    uint8_t processor_id;
+    uint16_t flags;
+    uint8_t lint;
+} __attribute__ ((packed));
