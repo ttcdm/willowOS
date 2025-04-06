712f8906c252a3a289872dffc0da4af92f507505
ttcdm <titus.gamerfpv@gmail.com>
2025-04-06 18:35:45 -0400

    it works sorta. i can halt thread execution and switch to another thread and resume later. still not sure why i can't put switch thread inside the scheduler loop instead of the interrupt handler tho

--- limine-c-template-x86-64/kernel/src/scheduler.asm
+++ limine-c-template-x86-64/kernel/src/scheduler.asm
@@ -39,20 +39,45 @@ pop_all_regs:
 [global switch_thread]
 ;.type switch_thread, @function
 switch_thread:
+    ;push rbx
+    ;push rbp
+    ;push r12
+    ;push r13
+    ;push r14
+    ;push r15
+
+    push rax
     push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rdi
     push rbp
+    push r8
+    push r9
+    push r10
+    push r11
     push r12
     push r13
     push r14
     push r15
 
-    mov rsp, (rdi)
-    mov rsi, rsp
+    ;mov rsp, (rdi)
+    mov rdi, rsp
 
     pop r15
     pop r14
     pop r13
     pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
     pop rbp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
     pop rbx
+    pop rax
     ret
