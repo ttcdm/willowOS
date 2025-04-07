e5cf4c6179507141dd813fccb5d9c48fea000ab3
ttcdm <titus.gamerfpv@gmail.com>
2025-04-06 23:17:05 -0400

    aa

--- limine-c-template-x86-64/kernel/src/scheduler.asm
+++ limine-c-template-x86-64/kernel/src/scheduler.asm
@@ -46,38 +46,23 @@ switch_thread:
     ;push r14
     ;push r15
 
-    push rax
     push rbx
-    push rcx
-    push rdx
-    push rsi
-    push rdi
     push rbp
-    push r8
-    push r9
-    push r10
-    push r11
     push r12
     push r13
     push r14
     push r15
 
-    mov rsp, rdi
+    ;mov rsp, rdi
     mov rdi, rsp
 
     pop r15
     pop r14
     pop r13
     pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
+
     pop rbp
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
+
     pop rbx
-    pop rax
+
     ret
