40b11714c319ce3884e0509c373c48c57f98df7d
ttcdm <titus.gamerfpv@gmail.com>
2025-04-13 05:31:10 -0400

    cleaned up init_scheduler by removing the create thread block and just put in the function instead. not sure why it wasn't working before but i think it works now. also added in the code from osdev discord

--- limine-c-template-x86-64/kernel/src/scheduler.c
+++ limine-c-template-x86-64/kernel/src/scheduler.c
@@ -8,7 +8,7 @@ void gen() {
 	kprint("\ngen0: hi from thread ");
 	// kpass(1000);
 	while (1){
-		int a = current_thread->pid;
+		int a = 0;
 		kprint("hi");
 		//kprint_uint64(current_thread->pid);
 		for (int i = 0; i < 10000000; i++) {
@@ -37,22 +37,9 @@ void init_scheduler() {
 
 	thread_context* current_thread;
 
-	thread_context* new_thread = (thread_context*)kmalloc_byte(sizeof(thread_context));
-	uint64_t* thread_base = kmalloc_byte(sizeof(uint64_t) * 2000);//16kb
-	new_thread->start_time = tsc_read_ns();
-	new_thread->last_start_time = 0;
-	new_thread->total_run_time = 0;
-	new_thread->quantum_ns = 10000000;//10ms
-	new_thread->pid = 0;
-	new_thread->thread_entry = gen;
-	new_thread->stack_base = thread_base;
-	new_thread->return_rsp = NULL;
-	new_thread->misaligned_by = 0;
-	new_thread->current_rsp = 0;
-	new_thread->current_misaligned_by = 0;
-	// new_thread->rip = NULL;
+	thread_context* new_thread = create_thread(0, gen);
 	new_thread->next_thread = NULL;
-
+	kprintln("hi");
 	current_thread = new_thread;
 	size_t num_threads = 5;
 	current_thread->next_thread = create_thread(1, gen);
@@ -69,6 +56,10 @@ void init_scheduler() {
 
 	//current_thread is basically the head??
 	
+	while (1) {
+		current_thread = current_thread->next_thread;
+		kprintln_uint64(current_thread->pid);
+	}
 	
 	//insert head of ready queue right after current_thread
 	thread_context* ready_queue_head = current_thread;
@@ -81,12 +72,37 @@ void init_scheduler() {
 
 }
 
-void reschedule()
 
-void scheduler_loop() {
-	while (1) {
+//thanks to mishakov for the code
+thread_context* ready_queue; // typically linked list for round robin scheduler
+thread_context* pop_front(thread_context*); // Removes the thread from the front and returns its pointer, or null if empty
+thread_context* push_back(thread_context*, thread_context*); // Pushes thread to the queue
+thread_context* get_current_thread(); // Returns the running thread
 
-	}
+void disable_preemption()
+{
+	asm volatile ("cli");
+}
+void enable_preemption()
+{
+	asm volatile ("sti");
+}
+
+void reschedule() {
+	disable_preemption();
+
+	thread_context* next_thread = pop_front(&ready_queue);
+	if (!next_thread)
+		goto end;
+
+	thread_context* current_thread = get_current_thread();
+	push_back(&ready_queue, current_thread);
+
+	switch_thread(current_thread, next_thread);
+	change_tss();
+
+end:
+	enable_preemption();
 }
 
 void scheduler_return() {
@@ -116,8 +132,10 @@ thread_context* create_thread(uint64_t pid, void (*thread_entry)(void)) {
 	new_thread->stack_base = thread_base;
 	new_thread->next_thread = NULL;
 
-    uint64_t thread_rsp = ((uint64_t) current_thread->stack_base) + THREAD_STACK_SIZE;//i'm not actually sure if kmalloc is supposed to return an address that's been casted to a pointer. either way, this reverts it so it should be okay for now i think
-	new_thread->current_rsp = thread_rsp;
+    //uint64_t thread_rsp = ((uint64_t) current_thread->stack_base) + THREAD_STACK_SIZE;//i'm not actually sure if kmalloc is supposed to return an address that's been casted to a pointer. either way, this reverts it so it should be okay for now i think
+	//new_thread->current_rsp = thread_rsp;
+	// new_thread->current_rsp;
+	// kprintln("hihi");
 	return new_thread;
 }
 
