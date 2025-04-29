90c2a09fc123f2f610bd166a86448cae68b22adb
ttcdm <titus.gamerfpv@gmail.com>
2025-04-29 12:37:40 -0400

    ioapic works somehow. all i did was zero out the struct members

--- limine-c-template-x86-64/kernel/src/ioapic.c
+++ limine-c-template-x86-64/kernel/src/ioapic.c
@@ -4,6 +4,17 @@ void init_ioapic(void) {
     //we only set up for ps/2 keyboard for now
     //irq 1 is used for keyboard interrupts
     //chatgpt said that we just use 00 as xy for the ioapic registers' memory addresses
+    
+    outb(0x20,0x11);
+    outb(0xA0,0x11);
+    outb(0x21,0x20);
+    outb(0xA1,0x28);
+    outb(0x21,0x2);
+    outb(0xA1,0x4);
+    outb(0x21,1);
+    outb(0xA1,1);
+    outb(0x21,0xFF);
+    outb(0xA1,0xFF);
     volatile uint32_t* ioapic_base = (uint32_t*) 0xfec00000;
     map_page((uint64_t*) pml4_address_virt_glob, (uint64_t) ioapic_base, (uint64_t) ioapic_base, 0b11);//always remember to map devices and stuff since we're not kmalloc'ing
     volatile uint32_t* ioapic_ioregsel = (uint32_t*) (ioapic_base);
@@ -13,6 +24,14 @@ void init_ioapic(void) {
 
     keyboard_redtbl.vector = 224;
     keyboard_redtbl.destination = 0;
+    keyboard_redtbl.mask = 0;
+
+    keyboard_redtbl.delivery_mode = 0;
+    keyboard_redtbl.destination_mode = 0;
+    keyboard_redtbl.delivery_status = 0;
+    keyboard_redtbl.pin_polarity = 0;
+    keyboard_redtbl.remote_irr = 0;
+    keyboard_redtbl.trigger_mode = 0;
 
     *ioapic_ioregsel = 0x10 + (irq * 2);//index for ioapic register (irq redirects);
     volatile uint32_t* keyboard_redtbl_low = (uint32_t*) (ioapic_iowin);//we write the low and then the high
@@ -22,7 +41,7 @@ void init_ioapic(void) {
                            (keyboard_redtbl.destination << 12) |
                            (keyboard_redtbl.delivery_status << 13) |
                            (keyboard_redtbl.pin_polarity << 14) |
-                           (keyboard_redtbl.remote_irr << 15) |
+                        //    (keyboard_redtbl.remote_irr << 15) |
                            (keyboard_redtbl.trigger_mode << 15) |
                            (keyboard_redtbl.mask << 16);
 
@@ -30,13 +49,15 @@ void init_ioapic(void) {
     volatile uint32_t* keyboard_redtbl_high = (uint32_t*) (ioapic_iowin);
     *keyboard_redtbl_high = (keyboard_redtbl.destination << 24);//we shift left because msb is on the left hand side
 
+    *ioapic_ioregsel = 0x10 + (irq * 2);
+    kprintf("%b\n", *keyboard_redtbl_low);
 
-    outb(0x21, 0xFF); // Master PIC mask all
-    outb(0xA1, 0xFF); // Slave PIC mask all
+    // outb(0x21, 0xFF); // Master PIC mask all
+    // outb(0xA1, 0xFF); // Slave PIC mask all
 
-    while (inb(0x64) & 0x01) {
-        inb(0x60);
-    }
+    // while (inb(0x64) & 0x01) {
+    //     inb(0x60);
+    // }
 
     outb(0x64, 0xAD); // Disable first PS/2 port (keyboard)
 outb(0x64, 0xA7); // Disable second PS/2 port (mouse, optional)
