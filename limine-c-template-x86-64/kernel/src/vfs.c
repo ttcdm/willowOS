a16a30d77450336e46dc16e865c57ef94595f9bc
ttcdm <titus.gamerfpv@gmail.com>
2025-05-08 04:58:38 -0400

    vfs mount works. i don't think it's the correct way to do it but it's the same idea but it works for now but i will actually properly implement it later. also trying to implement realloc_byte and vfs unmount

--- limine-c-template-x86-64/kernel/src/vfs.c
+++ limine-c-template-x86-64/kernel/src/vfs.c
@@ -20,8 +20,36 @@ void init_vfs() {
     kprintf("%s\n", buffer);
 
 
+
+    root->vnode_ops->vnode_mkdir(root, "test dir 3");
+
+    vnode_t* d = root->vnode_ops->vnode_lookup(root, "test dir 3");
+
+    vnode_mount_vfs(d, tmpfs);
+
+    vnode_t* f2 = d->vnode_ops->vnode_lookup(d, "TMPFS_ROOT");
+    kprintf("%s\n", ((tmpfs_header_t*)f2->vnode_data)->name);
+
+
+    tmpfs_list_files(f2->vnode_data);
+
+    // vnode_unmount_vfs
 }
 
+
+
+void vnode_mount_vfs(vnode_t* parent_vnode, vfs_t* vfs) {//this most likely isn't the correct way to implement it...
+    if (vfs == NULL) {
+        kprintf("vnode_mount_vfs(): vfs is null\n");
+    }
+    parent_vnode->mounted_vfs = vfs;
+}
+
+void vnode_unmount_vfs(vnode_t* parent_vnode, char* name) {
+    //need to lookup() inside the vnode
+    //we can either use a linked list approach and remove the element from the linked list and link the previous vfs to the next one
+    //or we can use an array approach and set the vfs to null but it'll also mean we need to implement realloc first
+}
 
 
 
