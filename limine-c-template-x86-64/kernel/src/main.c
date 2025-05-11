9d87fd5ce2626132a30de359d3116b298224780c
ttcdm <titus.gamerfpv@gmail.com>
2025-05-11 01:10:55 -0400

    changed init_mp to use a ticketing ish system and put mutexes/spinlocks around ft_ctx so prints from different cpus/threads don't have to fight over it. however, there isn't a ticketing system for who gets the lock first after it's released

--- limine-c-template-x86-64/kernel/src/main.c
+++ limine-c-template-x86-64/kernel/src/main.c
@@ -249,9 +249,13 @@ struct usable_memmaps_region* init_memmaps() {//HERE it's now every memmap there
 
 struct limine_framebuffer* framebuffer;
 struct flanterm_context* ft_ctx;
+mutex_t ft_ctx_mutex;
 
 uint64_t gdt_table[7];
 struct TSS tss __attribute__((aligned(16)));
+
+bool smp_init;
+uint64_t smp_ticket;
 
 
 size_t kstrlen(char* msg) {
@@ -305,7 +309,11 @@ void kprintf(char* fmt, ...) {
     // char* str = (char*) kmalloc_byte(size);//+1 byte for null terminating char. i don't think i actually need this because i'm using actual sizes instead of relying on the terminating char
     char str[size];//was told that using a variable length array was a bad idea...
     npf_vsnprintf(str, size+1, fmt, args_copy);//+1 byte for null terminating char. we need this because it assumes that the last thing is a null terminating char or something
+    
+    acquire_mutex(&ft_ctx_mutex);
     flanterm_write(ft_ctx, str, size);
+    release_mutex(&ft_ctx_mutex);
+
     // kfree((uint64_t*) str);
     va_end(args);
     va_end(args_copy);
@@ -321,7 +329,11 @@ void kprintf_interruptable(char* fmt, ...) {
     // char* str = (char*) kmalloc_byte(size);//+1 byte for null terminating char. i don't think i actually need this because i'm using actual sizes instead of relying on the terminating char
     char str[size];//should be fine
     npf_vsnprintf(str, size+1, fmt, args_copy);//+1 byte for null terminating char. we need this because it assumes that the last thing is a null terminating char or something
+    
+    acquire_mutex(&ft_ctx_mutex);
     flanterm_write(ft_ctx, str, size);
+    release_mutex(&ft_ctx_mutex);
+
     // kfree((uint64_t*) str);
     va_end(args);
     va_end(args_copy);
@@ -391,6 +403,9 @@ void kmain(void) {
 
     clear_framebuffer(framebuffer, BLACK);
 
+    ft_ctx_mutex.object = ft_ctx;
+    ft_ctx_mutex.locked = 0;
+
     // kprint("helloworld\n");
     kprintln("willowOS");
 
@@ -452,7 +467,10 @@ void kmain(void) {
 
     tsc_init();//don't put in interrupt because it sends a vector of the same priority twice and it doesn't continue or something
 
-    // init_mp(&mp_request);
+    smp_init = 0;
+    smp_ticket = 0;
+    init_mp(&mp_request);
+    smp_init = 1;
 
     // hpet is initialized inside init_bsp_lapic();
 
@@ -484,10 +502,12 @@ void kmain(void) {
 
     init_vfs();
 
-    init_scheduler();
+    // init_scheduler();
 
+    while (1) {
+        kprintf("HIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHI\n");
+    }
 
-    init_mutex();
 
 
 
@@ -551,6 +571,16 @@ void start_ap() {//remember to not call any non processor specific init function
     // if ((*lapic_id)>>24==1) {
     //     init_scheduler();
     // }
+
+    smp_ticket++;
+
+    while (!smp_init) {
+        asm volatile ("pause");
+    }
+
+    while (1) {
+        kprintf("BYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYEBYE");
+    }
 
     while (1) {asm volatile ("hlt");}
 }
