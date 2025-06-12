9411300ae013af619443310092f84f2b713d770e
ttcdm <titus.gamerfpv@gmail.com>
2025-06-12 04:47:00 -0400

    elfs should be loaded but still figuring out how to run it

--- limine-c-template-x86-64/kernel/src/loader.c
+++ limine-c-template-x86-64/kernel/src/loader.c
@@ -3,5 +3,45 @@
 void init_loader(vfs_fd_t* file) {
     kprintf("loader init\n\n");
 
+    //so basically, for nonrelocatable elfs, we just call alloc_frame() and map it to the appropriate addresses.
+    //the alloc_frame() bitmap already takes care of everything for us so it doesn't matter for virt addresses that are the same cuz alloc_frame() will put them at different phys addresses
+
+    //HERE REMEMBER TO SWAP OUT CR3
+
+    //we should probably copy out or save cr3 before we actually run anything for the first time
+
+    Elf64_Ehdr* ehdr = file->data;
+    for (uint64_t i = 0; i < ehdr->e_phnum; i++) {
+        Elf64_Phdr* phdr = (ehdr->e_phoff + (i * ehdr->e_phentsize)) + (uint64_t) file->data;
+        if (phdr->p_type == 1)  {//PT_LOAD
+            uint64_t segment_start;
+            if (phdr->p_memsz < 4096) {
+                segment_start = alloc_frame();
+                map_page((uint64_t*) pml4_address_virt_glob, segment_start, (uint64_t) phdr->p_vaddr, 0b111);
+            }
+            else if (phdr->p_memsz % 4096 == 0) {//it doesn't matter that the physical frames aren't contiguous because the virtual addresses are contiguous and it takes care of it so you can just memcpy everything in one go
+                segment_start = alloc_frame();
+                map_page((uint64_t*) pml4_address_virt_glob, segment_start, (uint64_t) phdr->p_vaddr, 0b111);
+                for (uint64_t j = 0; j < ((phdr->p_memsz / 4096) - 1); j++) {
+                    uint64_t next_frame = alloc_frame();
+                    map_page((uint64_t*) pml4_address_virt_glob, next_frame, (uint64_t) phdr->p_vaddr + (4096 * (j + 1)), 0b111);
+
+                }
+            }
+            else if ((phdr->p_memsz % 4096 != 0) && (phdr->p_memsz > 4096)) {//i think this overlaps with the if block above it
+                segment_start = alloc_frame();
+                map_page((uint64_t*) pml4_address_virt_glob, segment_start, (uint64_t) phdr->p_vaddr, 0b111);
+                for (uint64_t j = 1; j < (phdr->p_memsz / 4096) + 1; j++) {
+                    uint64_t next_frame = alloc_frame();
+                    map_page((uint64_t*) pml4_address_virt_glob, next_frame, (uint64_t) phdr->p_vaddr + (4096 * j), 0b111);
+                }
+            }
+
+            memcpy((void*) phdr->p_vaddr, (void*) ((ehdr->e_phoff + (i * ehdr->e_phentsize)) + (uint64_t) file->data), phdr->p_filesz);
+        }
+
+    }
+
+    // asm volatile ("jmp %0" : : "r" (ehdr->e_entry));
     
 }
--- limine-c-template-x86-64/kernel/src/loader.h
+++ limine-c-template-x86-64/kernel/src/loader.h
@@ -12,3 +12,39 @@
 #include <vfs.h>
 
 void init_loader(vfs_fd_t* file);
+
+
+//https://man7.org/linux/man-pages/man5/elf.5.html
+
+
+#define EI_NIDENT 16
+typedef uint64_t Elf64_Addr;
+typedef uint64_t Elf64_Off;
+
+typedef struct {
+    unsigned char e_ident[EI_NIDENT];//identifier for elf
+    uint16_t      e_type;
+    uint16_t      e_machine;
+    uint32_t      e_version;
+    Elf64_Addr    e_entry;
+    Elf64_Off     e_phoff;
+    Elf64_Off     e_shoff;
+    uint32_t      e_flags;
+    uint16_t      e_ehsize;
+    uint16_t      e_phentsize;
+    uint16_t      e_phnum;
+    uint16_t      e_shentsize;
+    uint16_t      e_shnum;
+    uint16_t      e_shstrndx;
+} Elf64_Ehdr;
+
+typedef struct {
+    uint32_t   p_type;
+    uint32_t   p_flags;
+    Elf64_Off  p_offset;
+    Elf64_Addr p_vaddr;
+    Elf64_Addr p_paddr;
+    uint64_t   p_filesz;
+    uint64_t   p_memsz;
+    uint64_t   p_align;
+} Elf64_Phdr;
