592cea25848984025d46ad93695d8222737b16a0
ttcdm <titus.gamerfpv@gmail.com>
2025-10-04 23:44:03 -0400

    removing gcc and binutils stuff. hopefully this doesn't break anything. trying to see if clang is easier to use

--- kernel/include/oa_hash/oa_hash.c
+++
@@ -1,3 +0,0 @@
-#include "./oa_hash/oa_hash.h"
-
-//oa_hash readme said to create this file
--- libc/include/bits/wctrans_t.h
+++
@@ -1,6 +0,0 @@
-#ifndef MLIBC_WCTRANS_T_H
-#define MLIBC_WCTRANS_T_H
-
-typedef unsigned long wctrans_t;
-
-#endif /* MLIBC_WCTRANS_T_H */
